56be12ca4545d2c5ff6b66a25384228397c1d7bd
David Turner <david.turner@whitelabel-loyalty.com>
2022-12-17 18:26:23 +0000

    day 11 finished - phewgit add .

--- day_11/main.py
+++ day_11/main.py
@@ -1,16 +1,30 @@
 from utils.objectmother import ObjectMother
 from monkey import Monkey
 from monkey import MonkeyDataWrapper
+from functools import reduce
+
+
+def play_single_round(round_number: int):
+    for monkey in monkeys:
+        while monkey.items:
+            monkey_index, worry_level = monkey.inspect_item()
+            monkeys[monkey_index].receive_item(worry_level)
+        print("Round ", round_number+1, [monkey for monkey in monkeys])
 
 
 if __name__ == "__main__":
-    monkeys_data = ObjectMother("input_test.txt").return_list_of_multi_line_objects(MonkeyDataWrapper.factory, "Monkey")
+    monkeys_data = ObjectMother("input.txt").return_list_of_multi_line_objects(MonkeyDataWrapper.factory, "Monkey")
 
     monkeys = []
     for monkey_data in monkeys_data:
         monkeys.append(Monkey(monkey_data.get_name(), monkey_data.get_starting_items(), monkey_data.get_operation(), monkey_data.get_test()))
 
-    print(monkeys)
-    print(monkeys[0].get_operation())
-    print(monkeys[0].get_test())
+    divisors = [monkey.get_divisor() for monkey in monkeys]
+    Monkey._modulo = reduce(lambda x, y: x * y, divisors)
 
+    for n in range(10000):
+        play_single_round(n)
+
+    sorted_list = sorted([monkey.get_inspected() for monkey in monkeys])
+    print(sorted_list)
+    print("ans: ", sorted_list[-1] * sorted_list[-2])
--- day_11/monkey.py
+++ day_11/monkey.py
@@ -1,3 +1,4 @@
+import operator
 from utils.datawrapper import DataWrapper
 
 
@@ -16,15 +17,24 @@ class MonkeyDataWrapper(DataWrapper):
         return str(self.data[2]).split("Operation: ")[1]
 
     def get_test(self) -> str:
-        return self.data[3] + self.data[4] + self.data[5]
+        return self.data[3] + " " + self.data[4] + " " + self.data[5]
 
 
 class Monkey:
-    def __init__(self, name: str, items: [], operation: str, test: str):
+    _modulo = -1  # 9699690
+
+    def __init__(self, name: str, items: [], operation_str: str, test: str):
         self.name = name
-        self.items = items
-        self.operation = operation
+        self.items = [int(x) for x in items]
+        self.operation_str = operation_str
         self.test = test
+        self._divisor = int(self.test.split(" ")[3])
+        self.monkey_if_true = int(self.get_test().split(" ")[9])
+        self.monkey_if_false = int(self.get_test().split(" ")[15])
+        self.inspected = 0
+
+    def __repr__(self):
+        return self.name + str(self.items) + " i:" + str(self.inspected)
 
     def get_name(self):
         return self.name
@@ -32,14 +42,51 @@ class Monkey:
     def get_items(self):
         return self.items
 
-    def get_operation(self):
-        return self.operation
+    def get_operation_str(self):
+        return self.operation_str
+
+    def get_inspected(self):
+        return self.inspected
 
     def get_test(self):
         return self.test
 
-    def __repr__(self):
-        return self.name + str(self.items)
+    def get_divisor(self):
+        return self._divisor
+
+    def get_operator_function(self):
+        result = operator.mul
+        if "+" in self.operation_str:
+            result = operator.add
+        return result
+
+    def get_new_worry_level(self, old_worry_level: int) -> int:
+        result = self.get_operator_function()(old_worry_level, self.get_param(old_worry_level))
+        result = result % Monkey._modulo
+        return result
+
+    def get_param(self, worry_level:int) -> int:
+        param = self.get_operation_str().split(" ")[4]
+        if param == "old":
+            param = worry_level
+
+        return int(param)
+
+    def inspect_item(self) -> (int, int):
+        self.inspected += 1
+        new_worry = self.get_new_worry_level(self.items.pop(0))
+        # new_worry //= 3 # part one
+        return self.get_monkey_to_throw_to(new_worry), new_worry
+
+    def get_monkey_to_throw_to(self, worry_level):
+        if worry_level % self._divisor == 0:
+            return int(self.get_test().split(" ")[9])
+        else:
+            return int(self.get_test().split(" ")[15])
+
+    def receive_item(self, worry_level):
+        self.items.append(worry_level)
+
 
 
 
